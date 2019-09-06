7ddacbd96d702e2e7744c6492bfe4452a9273d6b
Mike Henderyckx <mike.henderyckx@gmail.com>
2019-09-06 14:30:41 +0200

    Better examples in index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import get from "lodash/get";
 import { subtract } from "lodash-es";
 import { sum } from "lodash-es";
 
-console.log("Map:", _.map([1, 2, 3], number => number * 2));
+console.log("Map (multiply by 2)", _.map([1, 2, 3], number => number * 2));
 
 const person = { names: { first: "John", last: "Doe" } };
 console.log("get:", get(person, 'names.first', 'Anon'));
 
-console.log("Subtract:", subtract([a, b]));
+console.log("Subtract (100 - 20)", subtract(100, 20));
 
-console.log("Sum:", sum([a, b]));
+console.log("Sum (10 + 3)", sum([10, 3]));
